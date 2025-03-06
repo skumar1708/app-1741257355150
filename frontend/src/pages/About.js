73038b0e75db14fcce96cc058e018cc2e679f6af
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 16:12:00 +0530

    Automated commit: frontend/src/pages/About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about-page">
+      <h1>About Me</h1>
+      <p>Welcome to my travel blog. I am a passionate traveler and photographer.</p>
+    </div>
+  );
+}
+
+export default About;
