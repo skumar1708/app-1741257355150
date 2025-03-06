22521ff69d16116a2355598e5fb831aa16fb9122
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 16:10:11 +0530

    Automated commit: backend/routes/blogRoutes.js

---
+++ backend/routes/blogRoutes.js
@@ -0,0 +1,1 @@
+const express = require('express'); const { getBlogs, getBlogById, createBlog, updateBlog, deleteBlog } = require('../controllers/blogController'); const router = express.Router(); router.route('/').get(getBlogs).post(createBlog); router.route('/:id').get(getBlogById).put(updateBlog).delete(deleteBlog); module.exports = router;
