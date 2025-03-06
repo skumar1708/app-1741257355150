2eb408e60f5429072095ec15bc985b91f6ff16d0
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 16:09:47 +0530

    Automated commit: backend/models/Gallery.js

---
+++ backend/models/Gallery.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose'); const GallerySchema = new mongoose.Schema({ imageUrl: { type: String, required: true } }); module.exports = mongoose.model('Gallery', GallerySchema);
